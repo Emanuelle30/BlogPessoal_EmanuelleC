5312bf1b9356ef98889fb6cdc08e27d4cc4f856a
Emanuelle Cavalcante <emanuelle.cavalcante4@gmail.com>
2022-06-01 21:23:24 -0300

    implementação do consumo da API em cadastrar e entrar

---
+++ src/app/service/auth.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    
+    private http: HttpClient 
+    ) { }
+   
+
+  entrar(UsuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+    return this.http.post<UsuarioLogin>('https://blogpessoalemanuelle.herokuapp.com/usuarios/logar', UsuarioLogin)
+
+  }
+
+  cadastrar(usuario:Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://blogpessoalemanuelle.herokuapp.com/usuarios/cadastrar', Usuario)
+
+  }
+}
