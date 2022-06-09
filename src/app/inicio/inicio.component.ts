29fbf73df6ba2d0f5bdfc16d3800452067fc4648
Emanuelle Cavalcante <emanuelle.cavalcante4@gmail.com>
2022-06-09 18:03:32 -0300

    Implementação das Configs. de Postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -4,6 +4,7 @@ import { environment } from 'src/environments/environment.prod';
 import { Postagem } from '../model/Postagem';
 import { Tema } from '../model/Tema';
 import { Usuario } from '../model/Usuario';
+import { AuthService } from '../service/auth.service';
 import { PostagemService } from '../service/service-postagem.service';
 import { TemaService } from '../service/service-tema.service';
 
@@ -16,6 +17,7 @@ import { TemaService } from '../service/service-tema.service';
 export class InicioComponent implements OnInit {
  
   postagem: Postagem = new Postagem()
+  listaPostagens: Postagem []
 
   tema: Tema = new Tema()
   listaTemas: Tema[]
@@ -23,11 +25,14 @@ export class InicioComponent implements OnInit {
 
   usuario: Usuario = new Usuario()
   idUsuario = environment.id
+ 
 
   constructor(
     private router: Router,
     private postagemService: PostagemService,
-    private temaService: TemaService
+    private temaService: TemaService,
+    private auth: AuthService,
+    private authService: AuthService
   ) { }
 
   ngOnInit() {
@@ -37,7 +42,9 @@ export class InicioComponent implements OnInit {
       this.router.navigate(['/entrar'])
     }
 
-    this.getTema()
+    this.auth.refreshToken();
+    this.getTema();
+    this.getPostagens();
     }
 
     getTema(){
@@ -52,6 +59,20 @@ export class InicioComponent implements OnInit {
       })
 
     }
+
+    getPostagens(){
+      this.postagemService.getPostagens().subscribe((resp: Postagem[]) => {
+        this.listaPostagens = resp
+      })
+
+    }
+
+    findByIdUsuario(){
+      this.authService.getByIdUsuario(this.idUsuario).subscribe((resp: Usuario) =>{
+        this.usuario = resp
+      })
+    }
+
     publicar(){
       this.tema.id = this.idTema
       this.postagem.tema = this.tema
@@ -62,6 +83,8 @@ export class InicioComponent implements OnInit {
       this.postagemService.postPostagens(this.postagem).subscribe((resp: Postagem)=>{
         this.postagem = resp
         alert('Postagem realizada com sucesso!')
+        this.postagem = new Postagem()
+        this.getPostagens()
       })
     }
   }
--- src/app/service/service-postagem.service.ts
+++ src/app/service/service-postagem.service.ts
@@ -11,6 +11,7 @@ export class PostagemService {
 
   constructor(
     private http: HttpClient) { }
+    
 
 token = {
   headers: new HttpHeaders().set('Authorization', environment.token)
