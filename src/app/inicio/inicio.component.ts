17b7131fe67b2dca1a5c246e2618bc6eb7beaf88
Emanuelle Cavalcante <emanuelle.cavalcante4@gmail.com>
2022-06-08 23:16:39 -0300

    config iniciais do metodos de postagem

--- src/app/inicio/inicio.component.ts
+++ src/app/inicio/inicio.component.ts
@@ -1,6 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { Postagem } from '../model/Postagem';
+import { Tema } from '../model/Tema';
+import { Usuario } from '../model/Usuario';
+import { PostagemService } from '../service/service-postagem.service';
+import { TemaService } from '../service/service-tema.service';
+
 
 @Component({
   selector: 'app-inicio',
@@ -8,17 +14,57 @@ import { environment } from 'src/environments/environment.prod';
   styleUrls: ['./inicio.component.css']
 })
 export class InicioComponent implements OnInit {
+ 
+  postagem: Postagem = new Postagem()
+
+  tema: Tema = new Tema()
+  listaTemas: Tema[]
+  idTema: number
+
+  usuario: Usuario = new Usuario()
+  idUsuario = environment.id
 
   constructor(
-    private router: Router
+    private router: Router,
+    private postagemService: PostagemService,
+    private temaService: TemaService
   ) { }
 
   ngOnInit() {
+
     if(environment.token == ''){
       alert('Sua seção expirou, faça o login novamente!')
       this.router.navigate(['/entrar'])
     }
 
+    this.getTema()
+    }
+
+    getTema(){
+      this.temaService.getTema().subscribe((resp: Tema[]) => {
+        this.listaTemas = resp
+      })
+    }
+   
+    findByIdTema(){
+      this.temaService.getByIdTema(this.idTema).subscribe((resp: Tema) =>{
+        this.tema = resp
+      })
+
+    }
+    publicar(){
+      this.tema.id = this.idTema
+      this.postagem.tema = this.tema
+
+      this.usuario.id = this.idUsuario
+      this.postagem.usuario = this.usuario
+
+      this.postagemService.postPostagens(this.postagem).subscribe((resp: Postagem)=>{
+        this.postagem = resp
+        alert('Postagem realizada com sucesso!')
+      })
+    }
   }
 
-}
+
+
